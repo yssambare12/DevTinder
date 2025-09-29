9a5a0d16465fab95293ab5ed962c21318cdd4ae1
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-29 08:45:33 +0530

    chore: setup tailwind using vite

--- dev-tinder/src/App.jsx
+++ dev-tinder/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="p-4 m-4 bg-blend-darken text-3xl font-bold underline">
+        Vite + React
+      </h1>
     </>
   );
 }
